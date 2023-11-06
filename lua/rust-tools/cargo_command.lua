be542719130b915ecdd0a4f1a7b022e587726227
Ciel-MC <47144701+Ciel-MC@users.noreply.github.com>
2023-11-07 00:53:42 +0800

    feat(Cargo): Added cargo command wrapper that uses the current buffer instead of cwd
    It can be enabled by setting config.tool.cargo_wrapper to true.

--- lua/rust-tools/cargo_command.lua
+++ lua/rust-tools/cargo_command.lua
@@ -1,15 +1,32 @@
 local M = {}
 
+local manifest_path_argument = '--manifest-path'
+
+function M.register_ex_command()
+  vim.api.nvim_create_user_command('CargoWrapper', function(args)
+    local command = args.fargs
+    assert(command[1] == "cargo", "command was not cargo")
+    table.remove(command, 1)
+    M.cargo(command)
+  end, { nargs = '*' })
+end
+
+local opts = require("rust-tools.config").options
+
 function M.cargo(args)
-  require 'rust-tools.open_cargo_toml'.open_cargo_toml(function(_, result, ctx)
-    if result == nil then
-      vim.notify("Cargo.toml not found")
-    end
-    local location = vim.uri_to_fname(result.uri)
-    local command = vim.tbl_flatten{ 'cargo', args, '--manifest-path', location }
-    local val = vim.system(command):wait()
-    vim.print(val)
-  end)
+  if vim.tbl_contains(args, manifest_path_argument) then
+    -- vim.api.nvim_command('terminal ' .. table.concat(command, ' '))
+    opts.tools.executor.execute_command('cargo', args)
+  else
+    require 'rust-tools.open_cargo_toml'.open_cargo_toml(function(_, result, _)
+      if result == nil then
+        vim.notify("Cargo.toml not found")
+      end
+      local location = vim.uri_to_fname(result.uri)
+      args = vim.tbl_flatten { args, '--manifest-path', '"'..location..'"' }
+      opts.tools.executor.execute_command('cargo', args)
+    end)
+  end
 end
 
 return M
